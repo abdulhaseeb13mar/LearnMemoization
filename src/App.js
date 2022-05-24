9783d08d9bbda4b585beb3150bffb00aaa947bbc
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2022-05-24 18:35:31 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import Child from "./Child";
 
 function App() {
   const [parentNum, setParentNum] = useState(0);
   const [childNum, setChildNum] = useState(0);
+  const [arr, setArr] = useState([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ]);
+
+  const changeChildNumber = useCallback((num) => setChildNum(num), []);
 
   const incrementParentNum = () => {
     setParentNum((parentNum) => parentNum + 1);
+  };
+
+  const getLargestNumber = () => {
+    console.log("computing largest number");
+    return Math.max(...arr);
+  };
+
+  const largestNumber = useMemo(() => getLargestNumber(), [arr]);
+
+  // const changeChildNumber = (num) => {
+  //   setChildNum(num);
+  // };
+
+  const changeArray = () => {
+    setArr([100, 200, 400, 600, 500]);
   };
 
   return (
@@ -17,8 +37,10 @@ function App() {
           <button onClick={incrementParentNum}>
             Click to Increment parent number
           </button>
+          <h1>largest number: {largestNumber}</h1>
+          <button onClick={changeArray}>Change Array</button>
         </div>
-        <Child number={childNum} />
+        <Child changeNumber={changeChildNumber} number={childNum} />
       </div>
     </div>
   );
--- src/Child.js
+++ src/Child.js
@@ -1,15 +1,19 @@
-function Child({ number }) {
+import { memo } from "react";
+
+function Child({ number, changeNumber }) {
   console.log("Child rendered");
   return (
     <div className="r-c-c child-container">
       <div className="c-c-c parent">
         <div className="c-c-c">
           <h1>child number: {number}</h1>
-          <button>Click to Increment Child number</button>
+          <button onClick={() => changeNumber(parseInt(Math.random() * 10))}>
+            Click to Increment Child number
+          </button>
         </div>
       </div>
     </div>
   );
 }
 
-export default Child;
+export default memo(Child);
